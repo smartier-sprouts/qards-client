66c2ee57381fca7c34e2f01478f6e0bfe72384ed
r-i-c-h <rich@richwerden.com>
2017-07-26 11:11:51 -0700

    implements logout and (an as of the moment unused) verfication

---
+++ services/logout.js
@@ -0,0 +1,21 @@
+import {AsyncStorage} from 'react-native';
+
+export default async function logout(){
+    console.log('Logout Called');
+    const earlier = new Date("2000/01/01");
+
+    let infoObj = {
+      uID:"",
+      firstName: "",
+      fullName: "",
+      isLoggedIn: false,
+      expires: earlier,
+      token: null,
+    }
+
+    let jsonUserObj = JSON.stringify(infoObj);
+
+    AsyncStorage.setItem( 'asyncUserObj', jsonUserObj )
+    .catch( (e) => { console.error('Something went wrong in saving! - ', e); } );
+
+  }
