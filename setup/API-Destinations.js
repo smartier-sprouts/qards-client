48ce09fca17e3fb51e1bb7fbed04e8dcd8aa43bb
r-i-c-h <rich@richwerden.com>
2017-07-29 16:42:35 -0700

    Messed up the API addresses. Whoops.

--- setup/API-Destinations.js
+++ setup/API-Destinations.js
@@ -2,12 +2,12 @@ let api = {};
 
 api.main = 'https://qards.herokuapp.com/';
 api.socketServer = 'https://qards.herokuapp.com/';
-api.addPlayer =     api.main + '/api/addPlayer';
-api.getHand =       api.main + '/api/getHand/';
-api.getOpenGames =  api.main + '/api/games';
-api.checkDiscard =  api.main + '/api/discardChange/';
-api.discard =       api.main + '/api/discard/';
-api.pickUpCard =    api.main + '/api/drawCard/';
+api.addPlayer =     api.main + 'api/addPlayer';
+api.getHand =       api.main + 'api/getHand/';
+api.getOpenGames =  api.main + 'api/games';
+api.checkDiscard =  api.main + 'api/discardChange/';
+api.discard =       api.main + 'api/discard/';
+api.pickUpCard =    api.main + 'api/drawCard/';
 api.headers = { Accept: 'application/json', 'Content-Type': 'application/json' };
 
 module.exports = api;
