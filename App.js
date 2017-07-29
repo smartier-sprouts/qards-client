03df5b8184f4e4c7106b98adf47dbbb3fb0d641e
r-i-c-h <rich@richwerden.com>
2017-07-29 14:26:11 -0700

    First Rebasing Complete. Now on to next.

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, AsyncStorage, AppRegistry, Button, Picker, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+//import './setup/ReactotronConfig'; // <~~~ FOR DEBUGGING WITH REACTOTRON
 import Lobby from './components/Lobby.js';
-
-import verifyLoginStatus from './services/verifyLoginStatus.js';
-import logout from './services/logout.js';
 
 import WelcomeScreenTop from './components/WelcomeScreenTop.js';
 import WelcomeLogin from './components/WelcomeLogin.js';
+
+import verifyLoginStatus from './services/verifyLoginStatus.js';
+import logout from './services/logout.js';
 
 import styles from './styles/styles.js';
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -16,6 +16,7 @@ class Welcome extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isLoggedIn: false, isWaitingForAsync: true };
+
     this.checkStatus = this.checkStatus.bind(this);
   }
 
@@ -24,15 +25,10 @@ class Welcome extends React.Component {
   checkStatus() {
     verifyLoginStatus( this.necessaryCallback.bind(this) );
   }
-
   necessaryCallback(arg) { this.setState( {isWaitingForAsync: false, isLoggedIn: arg} ); }
 
   renderLoadingView() {
     return ( <View style={styles.bottomPart}><Text style={styles.smallTitle}>LOADING</Text></View>);
-  }
-
-  componentWillMount() {
-    this.setState({isLoggedIn: verifyLoginStatus() }, () => { console.log('LoginStatusOnWake:', this.state.isLoggedIn); });
   }
 
   renderLoggedInView() {
