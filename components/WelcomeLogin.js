bc3b17710a22ef7cff3812af0c0a207f440d863d
r-i-c-h <rich@richwerden.com>
2017-08-01 18:29:21 -0700

    Fixes Typos

--- components/WelcomeLogin.js
+++ components/WelcomeLogin.js
@@ -1,22 +1,30 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
-import styles from '../styles/styles.js';
-
+import { StyleSheet, Text, View, Button } from 'react-native';
 import fbFirebaseAuth from '../services/fbFirebaseAuth.js';
 
+import Spinner from './Spinner.js';
+import styles from '../styles/styles.js';
+
+
 export default class WelcomeLogin extends Component {
+  constructor(props){
+    super(props);
+    this.state = {isDoingBackgroundStuff: false}
+  }
+
+
   render() {
     return (
       <View style={styles.bottomPart}>
           <Button color='steelblue'
-            style={{ fontSize: 90 }}
-            onPress={
-                      async () => {
-                      console.log('off to fBook login');//
-                      await fbFirebaseAuth();
-                      this.props.chk();
-                    }} // forceUpdate() ?
             title = "LOGIN WITH FACEBOOK"
+            style = {{ fontSize: 90 }}
+            onPress = {
+              async () => {
+                await fbFirebaseAuth();
+                this.props.chk();
+              }
+            }
           />
       </View>
     );
--- services/fbFirebaseAuth.js
+++ services/fbFirebaseAuth.js
@@ -6,25 +6,22 @@ import {AsyncStorage} from 'react-native';
 import Keys from '../config/Keys';
 import * as firebase from 'firebase'
 
-
+export default async function fbFirebaseAuth() {
   const authenticate = (token) => {
-    const provider = firebase.auth.FacebookAuthProvider
-    const credential = provider.credential(token)
+    const provider = firebase.auth.FacebookAuthProvider;
+    const credential = provider.credential(token);
     return firebase.auth().signInWithCredential(credential);
   }
 
-  const fbFirebaseAuth = async () => {
-    const APP_ID = Keys.FACEBOOK_APP_ID;
-    const options = { permissions: ['public_profile', 'email'] }
-
-    const {type, token, expires} = await Expo.Facebook.logInWithReadPermissionsAsync(APP_ID, options);
+  const APP_ID = Keys.FACEBOOK_APP_ID;
+  const options = { permissions: ['public_profile', 'email'] }
+  const {type, token, expires} = await Expo.Facebook.logInWithReadPermissionsAsync(APP_ID, options);
 
     if (type === 'success') {
       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
       const profile = await response.json();
       const expirationDate = new Date(expires*1000);
       authenticate(token);
-
 
       let userObj = {
         uID:profile.id,
@@ -40,5 +37,3 @@ import * as firebase from 'firebase'
       .catch( (err) => { console.error('Err AsyncSaving Fbook User Data:- ', err); } );
     }
   }
-
-export default fbFirebaseAuth;
