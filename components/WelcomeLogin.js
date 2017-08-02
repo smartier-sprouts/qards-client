3b0e4af18ef85e048353647b7d6762ede33773e2
r-i-c-h <rich@richwerden.com>
2017-08-01 17:32:52 -0700

    Removes Google Button and references to earlier ggl/fbk auths

--- components/WelcomeLogin.js
+++ components/WelcomeLogin.js
@@ -2,30 +2,21 @@ import React, {Component} from 'react';
 import { StyleSheet, Text, View, AsyncStorage, Button } from 'react-native';
 import styles from '../styles/styles.js';
 
-import fbLogin from '../services/fbookExpoAuth.js';
-import gglLogin from '../services/googleExpoAuth.js';
+import fbFirebaseAuth from '../services/fbFirebaseAuth.js';
 
 export default class WelcomeLogin extends Component {
   render() {
     return (
       <View style={styles.bottomPart}>
           <Button color='steelblue'
-
+            style={{ fontSize: 90 }}
             onPress={
                       async () => {
                       console.log('off to fBook login');//
-                      await fbLogin();
+                      await fbFirebaseAuth();
                       this.props.chk();
                     }} // forceUpdate() ?
             title = "LOGIN WITH FACEBOOK"
-          />
-          <Button color='green'
-            onPress={ async () => {
-              await alert('To use Google Login, you must have Google Chrome installed on your phone!');
-              await gglLogin();
-              this.props.chk();
-            } }
-            title="LOGIN WITH GOOGLE"
           />
       </View>
     );
